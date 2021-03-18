f075862c3a4995869618f444a9de95e5383c0ed2
mccruddenw <wyatt.mccrudden@gmail.com>
2021-03-18 11:36:38 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code here!
+var main = document.getElementById("main"); 
+
+main.remove() 
+
+ 
+ 
+
+const newHeader = document.createElement('h1'); 
+
+newHeader.setAttribute('id', 'victory'); 
+
+document.body.appendChild(newHeader); 
+
+ 
+ 
+
+newHeader.innerHTML = "Wyatt is the champion!" 
+
+
